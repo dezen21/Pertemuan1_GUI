213d4ee719179b81d09b6171b25742f4624d3829
dezen21 <dezen21@gmail.com>
2019-04-02 17:13:33 +0700

    pertemuan GUI 1

---
+++ pertemuan1.py
@@ -0,0 +1,11 @@
+nilai = 70
+if (nilai >= 80):
+    print (" Nilai anda: A")
+elif (nilai <= 80):
+    print (" Nilai anda: B")
+elif (umur <= 70):
+    print (" Nilai anda: C")
+elif (umur <= 60):
+    print (" Nilai anda: D")
+else:
+    print (" Nilai anda: E")
